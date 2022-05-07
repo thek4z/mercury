4cccdb1c029c76f53e1a3728d274000b7a8c2d2f
thek4z <104161867+thek4z@users.noreply.github.com>
2022-05-07 14:59:43 +0200

    Update src.lua

--- src.lua
+++ src.lua
@@ -432,10 +432,10 @@ function Library:create(options)
 	end
 
 	options = self:set_defaults({
-		Name = "Mercury",
+		Name = "Kadium Hub",
 		Size = UDim2.fromOffset(600, 400),
 		Theme = self.Themes[settings.Theme],
-		Link = "https://github.com/deeeity/mercury-lib"
+		Link = "https://kadium/script/main"
 	}, options)
 
 	if getgenv and getgenv().MercuryUI then
